66abf3f230c2bd419879ad5b9cf9931060d1a373
MikaelCantini <mikaelac2.0@gmail.com>
2026-01-27 21:58:32 -0300

    Menu Finish

--- CarSystem2026/Program.cs
+++ CarSystem2026/Program.cs
@@ -8,19 +8,19 @@ public class Program
     public static void Main()
     {
 
-        Console.WriteLine("=====================");
-        Console.WriteLine("== CAR SYSTEM 2026 ==");
-        Console.WriteLine("=====================");
+        Console.WriteLine("=====================================");
+        Console.WriteLine("========   CAR SYSTEM 2026   ========");
+        Console.WriteLine("=====================================");
 
         bool isLoop = true;
         while (isLoop)
         {
-            Console.WriteLine("\n======  MENU  =======");
-            Console.WriteLine("\n1. Add Car in stock");
-            Console.WriteLine( "2. Remove Car from stock");
-            Console.WriteLine( "3. View Car List");
-            Console.WriteLine( "4. Exit Program");
-            
+            Console.WriteLine("\n================ MENU ================");
+            Console.WriteLine("\n  1. Add Car in stock");
+            Console.WriteLine( "  2. Remove Car from stock");
+            Console.WriteLine( "  3. View Car List");
+            Console.WriteLine( "  4. Exit Program");
+            Console.WriteLine("\n======================================");
             isLoop = false;
         }
     }
