d7868c49d32b813441c86fa5010163f8d0a0848d
MikaelCantini <mikaelac2.0@gmail.com>
2026-01-27 21:37:26 -0300

    Create Welcome Message

--- CarSystem2026/Program.cs
+++ CarSystem2026/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace carsystem2026;
+﻿using System.Linq.Expressions;
+
+namespace carsystem2026;
 
 public class Program
 {
     public static void Main()
     {
-        
+
+        Console.WriteLine("=====================");
+        Console.WriteLine("== CAR SYSTEM 2026 ==");
+        Console.WriteLine("=====================");
+
+        bool isLoop = true;
+        while (isLoop)
+        {
+            
+        }
     }
 }
