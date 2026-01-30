fd13023b662697bd7a8fe8e372d3c36e038811a4
MikaelCantini <mikaelac2.0@gmail.com>
2026-01-30 10:53:26 -0300

    Change Car attribute

--- CarSystem2026/Car.cs
+++ CarSystem2026/Car.cs
@@ -2,8 +2,8 @@ namespace carsystem2026;
 
 public class Car()
 {
-    public int Id;
-    public string Name;
-    public int Year;
-    public string Engine;
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public string Engine { get; set; }
 }
